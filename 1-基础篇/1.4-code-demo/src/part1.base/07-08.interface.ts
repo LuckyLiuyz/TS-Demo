06363bffb7a8fc9cbcef9908f177a02c86150865
LuckyLiuyz <281077012@qq.com>
2023-06-24 17:49:35 +0800

    函数和接口定义

--- 1-基础篇/1.4-code-demo/src/part1.base/07-08.interface.ts
+++ 1-基础篇/1.4-code-demo/src/part1.base/07-08.interface.ts
@@ -1,60 +1,88 @@
-interface List {
-    readonly id: number;
-    name: string;
-    // [x: string]: any;
-    age?: number;
-}
-interface Result {
-    data: List[]
-}
-function render(result: Result) {
-    result.data.forEach((value) => {
-        console.log(value.id, value.name)
-        if (value.age) {
-            console.log(value.age)
-        }
-        // value.id++
+{
+    interface List {
+        readonly id: number;// 只读属性：不允许修改
+        name: string;
+        age?: number; // 可选属性：既可有可无的属性
+        [x: string]: any; // 字符串索引签名：用任意的字符串去索引List，可以得到任意类型的结果
+    }
+    interface Result {
+        data: List[]
+    }
+    function render(result: Result) {
+        result.data.forEach((value) => {
+            console.log(value.id, value.name)
+            if (value.age) { // 如果List中不定义可选属性，此处就会报错
+                console.log(value.age)
+            }
+            // value.id++
+        })
+    }
+    let result = {
+        data: [
+            {id: 1, name: 'A', sex: 'male'},
+            {id: 2, name: 'B', age: 2}
+        ]
+    }
+    render(result);
+    render({
+        data: [
+            {id: 11, name: '1-A', sex: 'male'}, // 如果List不使用索引签名，此处会报错
+            {id: 12, name: '1-B', age: 12}
+        ]
     })
+    render({
+        data: [
+            {id: 21, name: '2-A', sex: 'male'},
+            {id: 22, name: '2-B', age: 22}
+        ]
+    } as Result) // 如果List不使用索引签名,可以使用类型断言，绕过检测也不报错
 }
-let result = {
-    data: [
-        {id: 1, name: 'A', sex: 'male'},
-        {id: 2, name: 'B', age: 10}
-    ]
-}
-render(result)
 
+// 数字索引的接口：如果不确定一个对象中有多少属性
 interface StringArray {
-    [index: number]: string
+    [index: number]: string // 用任意数字类型去索引StringArray，得到的是string类型的值
 }
 let chars: StringArray = ['a', 'b']
 
 interface Names {
-    [x: string]: any;
-    // y: number;
-    [z: number]: number;
+    // y: number; // 这是不被允许的
+    [x: string]: any; 
+    //[x: string]: string; // 此时会报错，因为下面数字索引签名的返回值定义的是number，不兼容
+    [z: number]: number; // 数字索引签名的返回值，必须是字符串索引签名返回值的子类型
 }
 
-// let add: (x: number, y: number) => number
-// interface Add {
-//     (x: number, y: number): number
-// }
-type Add = (x: number, y: number) => number
-let add: Add = (a: number, b: number) => a + b
+{
+    // 定义方式一
+    // let add: (x: number, y: number) => number
+    
+    // 定义方式二(等价与方式一)
+    // interface Add {
+    //     (x: number, y: number): number
+    // }
 
-interface Lib {
-    (): void;
-    version: string;
-    doSomething(): void;
+    // 定义方式三：类型别名，即type关键字，为当前函数类型起一个名字
+    type Add = (x: number, y: number) => number
+
+    let add: Add = (a, b) => a + b; //函数实现
+    console.log('add(1,2) =',add(1,2))
 }
 
-function getLib() {
-    let lib = (() => {}) as Lib
-    lib.version = '1.0.0'
-    lib.doSomething = () => {}
-    return lib;
+{
+    // 混合类型的接口：既可以定义函数，又可以定义普通字符属性等
+    interface Lib {
+        (): void;
+        version: string;
+        doSomething(): void;
+    }
+
+    function getLib() {
+        let lib = (() => {}) as Lib; // 如果不用类型断言，会报错。
+        lib.version = '1.0.0'
+        lib.doSomething = () => {}
+        return lib;
+    }
+    let lib1 = getLib()
+    lib1()
+    let lib2 = getLib()
+    lib2.doSomething()
 }
-let lib1 = getLib()
-lib1()
-let lib2 = getLib()
-lib2.doSomething()
--- 1-基础篇/1.4-code-demo/src/part1.base/09.function.ts
+++ 1-基础篇/1.4-code-demo/src/part1.base/09.function.ts
@@ -1,33 +1,48 @@
-// 函数定义
+// 函数定义的四种方式
 function add1(x: number, y: number) {
     return x + y
 }
+console.log('add1 =',add1(1, 2));
 
+
+// 以下3种必须满足：函数类型的定义 + 函数具体实现
 let add2: (x: number, y: number) => number
+add2 = (a, b) => a + b;
+console.log('add2 =',add2(1, 2));
 
-type add3 = (x: number, y: number) => number
 
-interface add4 {
+type ADD3 = (x: number, y: number) => number
+let add3: ADD3 = (a,b)=> a + b
+console.log('add3 =',add3(1, 2));
+
+interface ADD4 {
     (x: number, y: number): number
 }
+let add4: ADD4 = (a,b)=> a + b
+console.log('add4 =',add4(1, 2));
+// add4(1, 2, 3); // 此时会报错，提示add4只有两个参数，但是却传递了3个参数
 
-add1(1, 2, 3)
-
-function add5(x: number, y?: number) {
+// 可选函数参数:需要注意，必选参数必须位于可选参数之前
+function add5(x: number, y?: number /*, z: number*/) {
     return y ? x + y : x
 }
-add5(1)
+console.log('add5 =',add5(1));
+console.log('add5 =',add5(1,2));
+// add5(1,2,3);// 此时会报错，提示add5只接受1~2个参数，但是却传递了3个参数
 
-function add6(x: number, y = 0, z: number, q = 1) {
+// 必须参数之前有默认值的话，函数调用时必须通过传递undefined占位；必选参数之后如果有默认值，调用时可以不传
+function add6(x: number, y = 11, z: number, q = 1) {
     return x + y + z + q
 }
-add6(1, undefined, 3)
+console.log('add6 =',add6(1, undefined, 3)); // 16
 
+// 参数不固定的情况，使用【剩余参数】定义
 function add7(x: number, ...rest: number[]) {
     return x + rest.reduce((pre, cur) => pre + cur);
 }
-add7(1, 2, 3, 4, 5)
+console.log('add7 =',add7(1, 2, 3, 4, 5))
 
+// 函数重载：两个函数名称相同，但是参数不相同。要把最容易匹配的函数定义放在最前面，因为是从前往后匹配的。
 function add8(...rest: number[]): number;
 function add8(...rest: string[]): string;
 function add8(...rest: any[]) {
@@ -39,5 +54,5 @@ function add8(...rest: any[]) {
         return rest.join('');
     }
 }
-console.log(add8(1, 2))
-console.log(add8('a', 'b', 'c'))
+console.log('add8 =',add8(1, 2))
+console.log('add8 =',add8('a', 'b', 'c'))
